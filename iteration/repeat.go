85663b0e295c8a048310c74ada496769cffaa5ef
Leonardo Pinto <leoviveiros@gmail.com>
2021-01-30 11:51:27 -0300

    exercicio iteration

---
+++ iteration/repeat.go
@@ -0,0 +1,12 @@
+package iteration
+
+// Repeat repeats a character count times
+func Repeat(character string, count int) string {
+	var repeated string
+
+	for i := 0; i < count; i++ {
+		repeated += character
+	}
+
+	return repeated
+}
