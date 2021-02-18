fa8feafd3e0c99f1dc4525ce5e99b3bf3787b454
Leonardo Pinto <leoviveiros@gmail.com>
2021-02-18 11:17:49 -0300

    inclusao cli

---
+++ app/cmd/cli/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	poker "learn-go-with-tests/app"
+)
+
+const dbFileName = "game.db.json"
+
+func main() {
+    store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer close()
+    
+    fmt.Println("Let's play poker")
+    fmt.Println("Type {Name} wins to record a win")
+    
+    game := poker.NewCLI(store, os.Stdin)
+    game.PlayPoker()
+}
