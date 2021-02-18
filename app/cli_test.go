c6546d42ee170ea59a5400b22c2e00f81da7e53c
Leonardo Pinto <leoviveiros@gmail.com>
2021-02-18 11:17:49 -0300

    inclusao cli

--- app/cli_test.go
+++ app/cli_test.go
@@ -1,8 +1,10 @@
 package poker
 
 import (
+	"fmt"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestCLI(t *testing.T) {
@@ -10,7 +12,9 @@ func TestCLI(t *testing.T) {
         in := strings.NewReader("Chris wins\n")
         playerStore := &StubPlayerStore{}
 
-        cli := NewCLI(playerStore, in)
+        var dummySpyAlerter = &SpyBlindAlerter{}
+
+        cli := NewCLI(playerStore, in, dummySpyAlerter)
         cli.PlayPoker()
 
         assertPlayerWin(t, playerStore, "Chris")
@@ -20,9 +24,72 @@ func TestCLI(t *testing.T) {
         in := strings.NewReader("Cleo wins\n")
         playerStore := &StubPlayerStore{}
 
-        cli := NewCLI(playerStore, in)
+        var dummySpyAlerter = &SpyBlindAlerter{}
+
+        cli := NewCLI(playerStore, in, dummySpyAlerter)
         cli.PlayPoker()
 
         assertPlayerWin(t, playerStore, "Cleo")
     })
+
+    t.Run("it schedules printing of blind values", func(t *testing.T) {
+        in := strings.NewReader("Chris wins\n")
+        playerStore := &StubPlayerStore{}
+        blindAlerter := &SpyBlindAlerter{}
+
+        cli := NewCLI(playerStore, in, blindAlerter)
+        cli.PlayPoker()
+
+        cases := []struct{
+            expectedScheduleTime time.Duration
+            expectedAmount       int
+        } {
+            {0 * time.Second, 100},
+            {10 * time.Minute, 200},
+            {20 * time.Minute, 300},
+            {30 * time.Minute, 400},
+            {40 * time.Minute, 500},
+            {50 * time.Minute, 600},
+            {60 * time.Minute, 800},
+            {70 * time.Minute, 1000},
+            {80 * time.Minute, 2000},
+            {90 * time.Minute, 4000},
+            {100 * time.Minute, 8000},
+        }
+
+        for i, c := range cases {
+            t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedScheduleTime), func(t *testing.T) {
+
+                if len(blindAlerter.alerts) <= i {
+                    t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
+                }
+
+                alert := blindAlerter.alerts[i]
+
+                amountGot := alert.amount
+                if amountGot != c.expectedAmount {
+                    t.Errorf("got amount %d, want %d", amountGot, c.expectedAmount)
+                }
+
+                gotScheduledTime := alert.scheduledAt
+                if gotScheduledTime != c.expectedScheduleTime {
+                    t.Errorf("got scheduled time of %v, want %v", gotScheduledTime, c.expectedScheduleTime)
+                }
+            })
+        }
+    })
+}
+
+type SpyBlindAlerter struct {
+    alerts []struct{
+        scheduledAt time.Duration
+        amount int
+    }
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+    s.alerts = append(s.alerts, struct {
+        scheduledAt time.Duration
+        amount int
+    }{duration,  amount})
 }
